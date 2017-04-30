8510b730cd4edc10e49219c4d6eaac284fd51281
Krishna Deepika Villa <Deepika@krishnas-mbp.mobile.asu.edu>
2017-04-29 20:58:07 -0700

    pushing stored procedure for insertion into volunteer details

---
+++ Source Code/DML/spInsertIntoVolunteer_Details.sql
@@ -0,0 +1,20 @@
+if object_id ('spInsertIntoVolunteer_Details') IS NOT NULL
+drop proc spInsertIntoVolunteer_Details;
+go
+create proc spInsertIntoVolunteer_Details
+	@FirstName VARCHAR(50),
+	@LastName VARCHAR(50),
+	@ContactNo VARCHAR(20),
+	@EmailID VARCHAR(50),
+	@VolunteerService INT,
+	@TShirtSize INT,
+	@Address VARCHAR(255),
+	@NoOfDays INT,
+	@NoOfPeople INT
+
+as
+	begin
+	insert into Volunteer_Details 
+		(FirstName, LastName, ContactNo, EmailID, VolunteerService, TShirtSize, Address, NoOfDays, NoOfPeople)
+	values (@FirstName, @LastName, @ContactNo, @EmailID, @VolunteerService, @TShirtSize, @Address, @NoOfDays, @NoOfPeople)
+	end
