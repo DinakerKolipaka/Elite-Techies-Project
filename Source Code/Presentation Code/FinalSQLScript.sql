b74a7f47663c109a94b53a65b6251fe06f3f2e39
asamala <anusha.samala7@gmail.com>
2017-05-02 12:41:11 -0700

    revised1

--- Source Code/Presentation Code/FinalSQLScript.sql
+++ Source Code/Presentation Code/FinalSQLScript.sql
@@ -396,8 +396,8 @@ insert into Student_Volunteer_Mapping(StudentID,VolunteerID,ServiceProvided) val
 insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(1,'2017-07-08','2017-07-10',0);
 insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(2,'2017-07-26','2017-07-28',1);
 insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(3,'2017-07-29','2017-07-31',2);
-insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(4,'2017-07-22','2017-07-24',0);
-insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(5,'2017-07-29','2017-07-31',1);
+insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(4,'2017-08-01','2017-08-07',0);
+insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(5,'2017-07-29','2017-08-03',1);
 insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(6,'2017-08-03','2017-08-06',2);
 insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(7,'2017-08-01','2017-08-07',0);
 insert into Date_Ranges(VolunteerID,FromDate,ToDate,TypeOfService) values(8,'2017-07-09','2017-07-11',1);
